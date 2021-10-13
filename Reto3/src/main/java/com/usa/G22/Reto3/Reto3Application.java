82d9ff23d070fea71c4ec0fff0db949e93a5c484
NicolasMonroy1012 <91506577+NicolasMonroy1012@users.noreply.github.com>
2021-10-13 11:04:58 -0500

    ola

--- Reto3/src/main/java/com/usa/G22/Reto3/Reto3Application.java
+++ Reto3/src/main/java/com/usa/G22/Reto3/Reto3Application.java
@@ -2,14 +2,26 @@ package com.usa.G22.Reto3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class Reto3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Reto3Application.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
+			}
+		};
+	}
 }
 	
