3e4c450f6bf968db8eaef6d86b6698beedbcc840
Nicolas Monroy <91506577+NicolasMonroy1012@users.noreply.github.com>
2021-10-21 09:58:21 -0500

    final reto 4 ?

--- Reto3/src/main/java/com/usa/G22/Reto3/services/CategoryService.java
+++ Reto3/src/main/java/com/usa/G22/Reto3/services/CategoryService.java
@@ -43,7 +43,7 @@ public class CategoryService {
     }
     
     public Category update(Category category){
-        if(category.getId()==null){
+        if(category.getId()!=null){
             return categoryRepository.save(category);
         }else{
             Optional<Category> e=categoryRepository.getCategory(category.getId());
